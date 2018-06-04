06b12483ed4f27fbc2e5109898991e65fa3f76a6
wj844908240 <35714240+wj844908240@users.noreply.github.com>
2018-06-04 17:31:13 +0800

    Update run.py

--- app/run.py
+++ app/run.py
@@ -7,7 +7,7 @@ from config import db, app
 from api import api
 
 
-app.register_blueprint(api, url_prefix=settings.Api_Url_Prefix)
+app.register_blueprint(api)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
